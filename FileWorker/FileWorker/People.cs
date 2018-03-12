9aa9810780fae9bb8ebd07f310694ba51a548a5b
maxzzzzzzz <pankrationwolf@rambler.ru>
2018-03-12 17:42:09 +0200

    first commit

---
+++ FileWorker/FileWorker/People.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileWorker
+{
+    class People
+    {
+        int id;
+        string name;
+        string surname;
+        int age;
+        string email;
+        public People() { }
+        public People(int _id,string _name,string _surname, int _age, string _email)
+        {
+            id = _id;
+            name = _name;
+            surname = _surname;
+            age = _age;
+            email = _email;
+        }
+    }
+}
